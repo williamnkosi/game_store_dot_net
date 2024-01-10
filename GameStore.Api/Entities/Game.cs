c547eb7b8ad9e4b0e549d3f5f1aacc199897426d
William Nkosi <william.dev.acc@gmail.com>
2024-01-10 16:54:56 -0500

    initial setup of project

---
+++ GameStore.Api/Entities/Game.cs
@@ -0,0 +1,16 @@
+﻿namespace GameStore.Api.Entities
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; } = string.Empty;
+        public required string Genre { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public required string ImageUri { get; set; }
+
+    }
+}
---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,16 @@
+using GameStore.Api.Entities;
+
+List<Game> games = new()
+{
+    new Game() {Id =1 , Name= "Street Fighter II", Genre ="Fighting", Price = 19.99M, ReleaseDate = new DateTime(1991,2,1), ImageUri = "https://placehold.co/100"},
+    new Game() {Id =2 , Name= "Final Fantasy 14", Genre ="RolePlaying", Price = 59.99M, ReleaseDate = new DateTime(2010,9,30), ImageUri = "https://placehold.co/100"},
+    new Game() {Id =3 , Name= "FIFA 23", Genre ="Sports", Price = 69.99M, ReleaseDate = new DateTime(2022,5,24), ImageUri = "https://placehold.co/100"}
+};
+
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
