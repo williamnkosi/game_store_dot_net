12ded29bd44ee10f9b006492e87472ca1d06e17e
William Nkosi <william.dev.acc@gmail.com>
2024-01-15 04:08:41 -0500

    Added validation

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -13,7 +13,8 @@ List<Game> games = new()
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var group = app.MapGroup("/games");
+var group = app.MapGroup("/games")
+    .WithParameterValidation();
 
 group.MapGet("/", () => games);
 group.MapGet("/{id}", (int id) =>
